c999a4267b1654f9b1f03b959a5ae0b7c6546c89
danielmcaneiro <129326182+danielmcaneiro@users.noreply.github.com>
2023-04-16 02:07:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">TESTE PRÁTICA-2 ANTES DE CONFIGURAR O WEBHOOK</p></h1>'
+    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">TESTE PRÁTICA-2 WEBHOOK FUNCIONANDO PERFEITAMENTE!!</p></h1>'
 
 @app.route('/api')
 def hello():
